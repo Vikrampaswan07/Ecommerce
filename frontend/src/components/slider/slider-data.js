f6eeed93f901b235061b3abe8a2bc22a374bcd91
Vikrampaswan07 <130029534+Vikrampaswan07@users.noreply.github.com>
2024-08-17 00:06:12 +0530

    frontPage

---
+++ frontend/src/components/slider/slider-data.js
@@ -0,0 +1,29 @@
+// https://i.ibb.co/g4RDXbZ/phone2.jpg
+
+export const sliderData = [
+  {
+    image: "https://i.ibb.co/Sm79dMV/white-shoe.jpg",
+    heading: "Shoes Villa",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+  {
+    image: "https://i.ibb.co/Tw8mZVF/gadget.jpg",
+    heading: "Hot Gadgets",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+  {
+    image: "https://i.ibb.co/MsFgGcK/men-wear.jpg",
+    heading: "Men Fashion",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+  {
+    image: "https://i.ibb.co/c3C74c3/phone.jpg",
+    heading: "Android Phones",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+  {
+    image: "https://i.ibb.co/GCYhBhC/blue-shoe.jpg",
+    heading: "Nike Sneakers",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+];

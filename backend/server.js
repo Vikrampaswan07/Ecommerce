546945adbbb9caacd4f513bb263eea777355100a
Vikrampaswan07 <130029534+Vikrampaswan07@users.noreply.github.com>
2024-08-29 20:33:51 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,39 @@
-console.log("Hello World!!!");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
+
+const app = express();
+
+// MiddleWares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "https://shopitoapp.vercel.app"],
+        credentials: true,
+    })
+);
+
+// Route
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+    res.send("Home Page...")
+})
+
+// Error Middleware
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
+    .catch((err) => console.log(err))
